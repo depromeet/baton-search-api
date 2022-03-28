61904f26ce1408920957344f328e90cc78bc9cbb
justkode <sobu0715@gmail.com>
2022-03-29 00:30:11 +0900

    feat: Initial Commit

---
+++ src/main/java/depromeet/batonsearch/BatonSearchApplication.java
@@ -0,0 +1,13 @@
+package depromeet.batonsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatonSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatonSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/depromeet/batonsearch/BatonSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package depromeet.batonsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatonSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
