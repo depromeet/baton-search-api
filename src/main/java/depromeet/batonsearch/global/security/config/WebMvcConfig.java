c30150c52086997ad47ffe4309b014982700939b
justkode <sobu0715@gmail.com>
2022-04-01 01:40:56 +0900

    feat: Swagger Configuration

---
+++ src/main/java/depromeet/batonsearch/global/security/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package depromeet.batonsearch.global.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/WEB-INF/resources/");
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
