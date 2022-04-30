f66f696bd53c1ca53804262dd93eec410936134c
justkode <sobu0715@gmail.com>
2022-04-30 17:04:57 +0900

    feat: Distance Query

--- src/main/java/depromeet/batonsearch/global/config/MySQLDialect.java
+++ src/main/java/depromeet/batonsearch/global/config/MySQLDialect.java
@@ -1,11 +1,15 @@
 package depromeet.batonsearch.global.config;
 
+import org.hibernate.spatial.dialect.mysql.MySQL8SpatialDialect;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.type.BooleanType;
 import org.hibernate.type.IntegerType;
+import org.hibernate.type.StandardBasicTypes;
 
-public class MySQLDialect extends org.hibernate.dialect.MySQLDialect {
+public class MySQLDialect extends org.hibernate.spatial.dialect.mysql.MySQL8SpatialDialect {
     public MySQLDialect() {
         super();
+        registerFunction("geocontains", new SQLFunctionTemplate(BooleanType.INSTANCE, "MBRContains(st_linestringfromtext(?1), ?2)"));
         registerFunction("bitand", new SQLFunctionTemplate(IntegerType.INSTANCE, "(?1 & ?2)"));
     }
 }
