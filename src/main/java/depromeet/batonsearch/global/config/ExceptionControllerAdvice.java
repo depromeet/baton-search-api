c2ea00ca3fee7313edf6be3b3a078a94bf5ddedb
justkode <sobu0715@gmail.com>
2022-05-09 16:18:41 +0900

    fix: ExceptionControllerAdvice Scope

--- src/main/java/depromeet/batonsearch/global/config/ExceptionControllerAdvice.java
+++ src/main/java/depromeet/batonsearch/global/config/ExceptionControllerAdvice.java
@@ -6,7 +6,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
