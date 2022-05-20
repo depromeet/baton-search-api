92a6166ae7613f467d322bab81c486079a306615
justkode <sobu0715@gmail.com>
2022-05-21 03:03:46 +0900

    feat: Swagger Config

--- src/main/java/depromeet/batonsearch/BatonSearchApplication.java
+++ src/main/java/depromeet/batonsearch/BatonSearchApplication.java
@@ -2,7 +2,15 @@ package depromeet.batonsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+
+@EnableWebMvc
 @SpringBootApplication
 public class BatonSearchApplication {
 
@@ -10,4 +18,12 @@ public class BatonSearchApplication {
 		SpringApplication.run(BatonSearchApplication.class, args);
 	}
 
+	@Bean
+	public Docket swagger() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
