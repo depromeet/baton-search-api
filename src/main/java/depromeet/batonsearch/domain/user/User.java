ec5c85a68e0e7c7831e486a6b3f7ecb3d521ba89
justkode <sobu0715@gmail.com>
2022-06-04 23:35:14 +0900

    fix: User Column (add created, remove gender)

--- src/main/java/depromeet/batonsearch/domain/user/User.java
+++ src/main/java/depromeet/batonsearch/domain/user/User.java
@@ -5,6 +5,7 @@ import depromeet.batonsearch.domain.buy.Buy;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,11 +20,17 @@ public class User {
     @Id
     private Integer id;
 
-    @Column
-    private String nickname;
+    @Column(nullable = false)
+    private String name;
 
     @Column(nullable = false)
-    private Boolean gender;
+    private String nickname;
+
+    @Column(nullable = false, name = "phone_number")
+    private String phoneNumber;
+
+    @Column(nullable = false, name = "created_on")
+    private LocalDate createdOn;
 
     @OneToMany(mappedBy = "id")
     private Set<Bookmark> bookmarks;
